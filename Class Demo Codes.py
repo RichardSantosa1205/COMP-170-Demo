300225e2ea68bfa9a438b9f471e911946ba2ea94
Richard Zhang Santosa <rsantosa@luc.edu>
2025-07-02 17:46:58 +0000

    June code Update

--- Class Demo Codes.py
+++ Class Demo Codes.py
@@ -440,3 +440,52 @@ def our_upper_case(word:str) -> str:
 # quick demo
 print(our_upper_case("Leo9"))
 
+def our_upper_case(word:str) -> str:
+    result = None
+    if len(word) > 0:
+        result = ""
+        for i in range(len(word)):
+            current_character = word[i]
+            # find the ascii value of the current character
+            current_ascii = ord(current_character)
+            # Is this ascci value a letter? Is it between a-thru-z
+
+            if current_ascii >= ord('a') and current_ascii <= ord('z'):
+                # Subtract to bring to upper case
+                new_ascii = current_ascii - 32
+                # create a new symbol with this ascii code
+                new_character = chr(new_ascii)
+            else:
+                new_character = current_character
+                
+            # concatenate new char to the output string
+            result = result + new_character
+    # done
+    return result
+
+# quick demo
+print(our_upper_case("leo 1967"))
+
+#June 30 Code
+# example with full path to file
+
+with open("/workspaces/comp-170-su25-live-coding/demo_data/text_file.txt", "r") as leo_masterpiece:
+    opening_from = leo_masterpiece.read()
+    print(opening_from) # OK, maybe William Shakespeare helped a bit too.
+
+# Same but better organized ... this time it counts lines instead of just printing them
+
+path_to_file = "/workspaces/comp-170-su25-live-coding/demo_data/"
+file_name =  "text_file.txt"
+line_count = 0
+with open(path_to_file + file_name) as richard3:
+    for line in richard3:
+        line_count += 1 
+print(f"\n\nThere are {line_count} line(s) in file {file_name}.\n")
+
+# Try a file in the same folder as our program
+path_to_file = "./" # File system speak for "same folder"
+file_name =  "README.md"
+with open(path_to_file + file_name) as local_file:
+    contents_of_local_file = local_file.read()
+    print(f"\n\nContents of {file_name}:\n\n{contents_of_local_file}\n")
